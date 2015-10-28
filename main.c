e59827db72b18cb9893b9cfcdd3911b3e3f9aa9e
Apollon <Apollon>
2015-10-28 16:26:39 -0400

    vague structure setup

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "main.h"
+
+int main(int argc, void *argv) {
+	// TODO: load file specified in first parameter
+	
+	// TODO: parse and store graph in graph
+	graph_p *graph = malloc(sizeof(graph_t));
+	
+	// TODO: implement command line like interface to receive commands from user
+	
+	return 0;
+}
