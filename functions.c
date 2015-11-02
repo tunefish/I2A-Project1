fec166e8019f61565e1e0ab4093e1ef035bce77d
tunefish <tunefish@users.noreply.github.com>
2015-11-02 17:43:50 -0500

    Adding comments, removed unneccesary code

--- functions.c
+++ functions.c
@@ -7,39 +7,45 @@ vertex_p find_vertex_by_name(graph_p graph, char *sp);
 
 queue_p num_children(graph_p graph, char *sp, int order, int num) {
     if (order < 1) {
+        // error in input: get direct neighbors
         order = 1;
     }
     if (num < 1) {
+        // all values less than one: get all matching species
         num = -1;
     }
     
     vertex_p v = find_vertex_by_name(graph, sp);
-    int stack_size = 1;
 
     if (!v) {
+        // initial species not found
         return NULL;
     }
 
-    int children = 0;
+    // reset depth and visited flags for all vertices
     reset(graph);
     queue_p result = (queue_p) malloc(sizeof(queue_t));
     
     queue_p queue = create_queue();
     enqueue(queue, v);
     
+    // classic BFS:
+    // repeat as long as there are more species to discover or we reached max wanted results (for all results: num < 0)
     while (!is_empty(queue) && num != 0) {
         v = dequeue(queue);
         edge_p e = v->neighbors;
         
         do {
-            enqueue(result, e);
-            num--;
-            
-            if (v->depth < order - 1) {
-                e->to->depth = v->depth + 1;
-                enqueue(queue, e->to);
+            if (!e->to->visited) {
+                enqueue(result, e);
+                num--;
+                
+                if (v->depth < order - 1) {
+                    e->to->depth = v->depth + 1;
+                    enqueue(queue, e->to);
+                }
             }
-        } while ((e = e->next) && num != 0);
+        } while ((e = e->next) && num != 0); // repeat for all neighbors until max number of results is reached
     }
 
     return result;
