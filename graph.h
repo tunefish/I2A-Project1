df346cfb59bcfc0b863fa50a646ff7eb0a0dad3d
tunefish <tunefish@users.noreply.github.com>
2015-11-02 16:28:49 -0500

    (Hopefully) final restructuring of graph

--- graph.h
+++ graph.h
@@ -4,20 +4,18 @@
 typedef struct vertex {
     void *data;                 // pointer to data represented by this vertex
     int visited;                // visited flag
+    struct edge *neighbors;     // pointer to first element of linked list of neighbors
     struct vertex *next;        // pointer to next vertex in vertex list
 } vertex_t, *vertex_p;
 
 typedef struct edge {
-    vertex_p from;              // start vertex of this edge
     vertex_p to;                // end vertex of this edge
     struct edge *next;          // pointer to next edge in edge list
 } edge_t, *edge_p;
 
 typedef struct graph {
     int num_vertices;           // total number of vertices
-    int num_edges;              // total number of vertices
     vertex_p vertices;          // pointer to first element of linked list of vertices
-    edge_p edges;               // pointer to first element of linked list of edges
 } graph_t, *graph_p;
 
 typedef struct species {
